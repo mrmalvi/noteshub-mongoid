d7ef114ab73a4ef8c1e0995afd5d4dfd7f870b8f
mrmalvi <malviyak00@gmail.com>
2025-11-06 17:01:37 +0530

    add header footer

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,5 +4,11 @@ module Users
       puts params[:user][:login] = params[:user][:login].downcase if params[:user][:login]
       super
     end
+
+    protected
+
+    def configure_sign_in_params
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    end
   end
 end
